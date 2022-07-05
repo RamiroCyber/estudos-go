0614ce948a4292ce277b80d3b67e35584794e4e8
RamiroCyber <ramiro.dr@gmail.com>
2022-07-04 22:42:33 -0300

    criando metodos

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/estudodb/entities"
 	"github.com/gorilla/mux"
-
+	"net/http"
 )
 
-var movies = []entities.Movie
+var movies = []entities.Movie{}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	parans := mux.Vars(r)
+	for index, movie := range movies {
+
+		if movie.ID == parans["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/movies",getMovies).Methods("GET")
-	r.HandleFunc("/movie/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", creatMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	http.ListenAndServe(":8080", r)
 }
